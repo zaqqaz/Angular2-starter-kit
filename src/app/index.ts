0db808e3ce457cc5cf8896430e491f8401990660
Denis <i@denis.by>
2016-04-09 18:30:59 +0300

    Api implementation

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,9 +4,9 @@ import {bootstrap} from 'angular2/platform/browser';
 import {enableProdMode, provide, Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 import {HTTP_PROVIDERS} from 'angular2/http';
-import {TypingCarouselDirective} from 'ng2-typing-carousel';
+import {Api, ApiConfig} from 'angular2-rest-api';
 import {router} from './router';
-//import {apiConfiguration} from './api/api.configuration';
+import {apiConfiguration} from './api/api.configuration';
 
 enableProdMode();
 
@@ -16,11 +16,13 @@ enableProdMode();
     template: '<router-outlet></router-outlet>',
 })
 @RouteConfig(router.config)
+@ApiConfig(apiConfiguration)
 class AppComponent {
 }
 
 bootstrap(AppComponent, [
     HTTP_PROVIDERS,
     ROUTER_PROVIDERS,
+    Api,
     provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
